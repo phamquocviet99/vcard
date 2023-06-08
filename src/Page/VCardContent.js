81955cf4704692079c65099d204081ae71b1e8c3
quangtaocaiboong <laquanghuy231100@gmail.com>
2023-06-08 16:28:41 +0700

    [feat]: final

---
+++ src/Page/VCardContent.js
@@ -0,0 +1,40 @@
+import VCard from "vcard-creator";
+
+export function SaveCard() {
+  const myVCard = new VCard();
+
+  // Some variables
+  const lastname = "";
+  const firstname = "Trần Thị Hữu Ái";
+  const additional = "";
+  const prefix = "";
+  const suffix = "";
+
+  myVCard
+    // Add personal data
+    .addName(firstname, lastname, additional, prefix, suffix)
+    // Add work data
+    .addCompany("Công ty Cổ Phần Flower Marketplace - FMP")
+    .addJobtitle("Giám đốc")
+    .addEmail("ctyfmp@gmail.com")
+    .addPhoneNumber(84983277941, "PREF;WORK")
+    .addAddress("1196 đường 3/2, phường 8, Quận 11, TP.HCM");
+
+  // Tạo Blob từ nội dung vCard
+  const blob = new Blob([myVCard], { type: "text/vcard" });
+
+  // Tạo URL để tải xuống tệp tin vCard
+  const url = window.URL.createObjectURL(blob);
+
+  // Tạo một thẻ <a> ẩn để thực hiện tải xuống
+  const downloadLink = document.createElement("a");
+  downloadLink.href = url;
+  downloadLink.download = "contact.vcf";
+
+  // Thêm thẻ <a> vào DOM và tự động kích hoạt sự kiện click để tải xuống
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+
+  // Xóa thẻ <a> sau khi tải xuống hoàn tất
+  document.body.removeChild(downloadLink);
+}
--- src/Page/Vcard.jsx
+++ src/Page/Vcard.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { BsFillTelephoneFill, BsPersonWorkspace } from "react-icons/bs";
+import { AiFillPhone, AiOutlineUserAdd } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
-import { TbWorld } from "react-icons/tb";
-import { AiFillPhone } from "react-icons/ai";
 import { HiOutlineMail, HiLocationMarker } from "react-icons/hi";
+import { SaveCard } from "./VCardContent";
 
-import { BiSolidShoppingBag } from "react-icons/bi";
 export default function Vcard() {
   return (
     <div className="block 2xl:flex justify-center">
@@ -22,28 +21,33 @@ export default function Vcard() {
             <p className="text-white font-bold text-center text-3xl mt-5">
               Trần Thị Hữu Ái
             </p>
-            <p className="text-white font-medium text-center text-xl mt-3">
+            <p className="text-white font-medium text-center text-xl mt-5">
               Giám đốc
             </p>
-            <div className="flex justify-center flex-wrap items-center text-white w-fit mx-auto mt-14 mb-10">
+            <div className="flex justify-center flex-wrap items-center text-white w-fit mt-14 mx-5 mb-10">
               <a
                 href="tel:0983277941"
                 className="flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
                 <BsFillTelephoneFill className="text-base" />
-                <p className="text-base">PHONE</p>
+                <p className="text-base">Call</p>
               </a>
               <a
                 href="mailto: ctyfmp@gmail.com"
                 className="flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
                 <MdEmail className="text-xl" />
-                <p className="text-base">EMAIL</p>
+                <p className="text-base">Email</p>
               </a>
-              <button className="flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+              <a
+                href="https://www.google.com/maps/place/1196+%C4%90.+3+Th%C3%A1ng+2,+Ph%C6%B0%E1%BB%9Dng+8,+Qu%E1%BA%ADn+11,+Th%C3%A0nh+ph%E1%BB%91+H%E1%BB%93+Ch%C3%AD+Minh+72614,+Vi%E1%BB%87t+Nam/@10.7609986,106.6395837,15.61z/data=!4m23!1m16!4m15!1m6!1m2!1s0x31752e8d64ba46d9:0x5ee4c9cbff93b10d!2zMTE5NiDEkMaw4budbmcgMyBUaMOhbmcgMiwgcGjGsOG7nW5nIDgsIFF14bqtbiAxMSwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5o!2m2!1d106.6502521!2d10.758934!1m6!1m2!1s0x31752959acf41157:0xd5e56d11828ec468!2zNDIgSOG7kyDEkOG6r2MgRGksIFTDonkgVGjhuqFuaCwgVMOibiBQaMO6LCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmggNzIwMTAsIFZp4buHdCBOYW0!2m2!1d106.632115!2d10.8061264!3e0!3m5!1s0x31752e8d64ba46d9:0x5ee4c9cbff93b10d!8m2!3d10.758934!4d106.6502521!16s%2Fg%2F11mk6_6fxq?entry=ttu"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
                 <HiLocationMarker className="text-xl" />
-                <p className="text-base">LOCATION</p>
-              </button>
+                <p className="text-base">Location</p>
+              </a>
             </div>
           </div>
         </div>
@@ -115,9 +119,12 @@ export default function Vcard() {
               className="bg-gray-300 w-full mt-3"
             />
             <div className="flex justify-center my-5">
-              <button className="py-3 px-4 rounded-lg text-white bg-[#f25b18] hover:bg-orange-700 font-medium">
+              <button
+                onClick={()=> SaveCard()}
+                className="py-3 px-4 flex items-center justify-center gap-2 rounded-lg text-white bg-[#f25b18] hover:bg-orange-700 font-medium"
+              >
                 {" "}
-                + Thêm liên hệ
+                <AiOutlineUserAdd className="text-xl"/> Thêm liên hệ
               </button>
             </div>
           </div>
